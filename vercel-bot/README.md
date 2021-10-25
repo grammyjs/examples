b0902ba9f8736eff6415a48a0b6e25423f1755c9
linbuxiao <linbuxiao@gmail.com>
2021-10-25 20:00:03 +0800

    fix: setWebhook when it deploy

--- vercel-bot/README.md
+++ vercel-bot/README.md
@@ -3,3 +3,5 @@
 You can deploy your bot as a webhook in vercel. Here are sample bots:
 
 https://t.me/vercel_grammy_bot
+
+Please run the index.js file in vercel's package command to set your webhook address.
--- vercel-bot/api/index.ts
+++ vercel-bot/api/index.ts
@@ -1,13 +1,8 @@
 import { Bot, webhookCallback } from 'grammy'
 
-const bot = new Bot('') // <-- Set your token in the vercel environment variable and call it with `process.env`
 
-// Set an option to repeat what you said.
-bot.on("message", async ctx => {
-  await ctx.reply(ctx.msg.text)
-})
+const bot = new Bot(process.env['BOT_TOKEN'])
 
-export default async (req, res) => {
-  await bot.api.setWebhook('') // <-- Fill in the address of your deployed vercel allocation.
+export default async (req, res) => { // <-- Fill in the address of your deployed vercel allocation.
   webhookCallback(bot, 'http')(req, res) // <-- The free version of vercel has restrictions on quotas, which we need to enable in the configuration file vercel.json
 }
