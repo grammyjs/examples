e5f76546276f9ec08244923433a45c7c597147dd
Walid <hello@walidvb.com>
2021-12-07 20:16:56 +0100

    working version of vercel app

--- vercel-bot/api/index.ts
+++ vercel-bot/api/index.ts
@@ -3,6 +3,11 @@ import { Bot, webhookCallback } from 'grammy'
 
 const bot = new Bot(process.env['BOT_TOKEN'])
 
-export default async (req, res) => { // <-- Fill in the address of your deployed vercel allocation.
-  webhookCallback(bot, 'http')(req, res) // <-- The free version of vercel has restrictions on quotas, which we need to enable in the configuration file vercel.json
-}
+// attach all middleware
+bot.on("message", (ctx) => {
+  ctx.reply("Hi there!")
+})
+
+// The free version of vercel has restrictions on quotas, which we need to enable in the configuration file vercel.json
+// webhookCallback will make sure that the correct middleware(listener) function is called
+export default webhookCallback(bot, 'express')(req, res)
