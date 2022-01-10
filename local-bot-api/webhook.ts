c0051a4d69a9e5982ae47ac9730b2c789f033dad
deptyped <deptyped@gmail.com>
2022-01-11 00:38:01 +0200

    Remove unnecessary manual creation of client agent

--- local-bot-api/webhook.ts
+++ local-bot-api/webhook.ts
@@ -1,5 +1,4 @@
 import { Bot, webhookCallback } from 'grammy'
-import { Agent } from 'http'
 import express from 'express'
 
 // 1. Create a bot
@@ -7,10 +6,6 @@ export const bot = new Bot(process.env.BOT_TOKEN as string, {
     client: {
         // 2. Set the local Bot API URL
         apiRoot: 'http://bot-api:8081',
-        baseFetchConfig: {
-            compress: true,
-            agent: new Agent({ keepAlive: true }),
-        },
     },
 })
 
