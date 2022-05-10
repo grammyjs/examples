dec049c0bb7a52d53f36f459bd20014890f598e4
Loskir <kl@sinker.ru>
2022-05-10 20:01:02 +0300

    Add Deno Deploy

---
+++ deno-deploy/server.ts
@@ -0,0 +1,23 @@
+import {
+  serve,
+} from "https://deno.land/x/sift@0.5.0/mod.ts";
+import { bot } from './bot.ts'
+import { webhookCallback } from "./deps.deno.ts";
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+serve({
+  ['/' + Deno.env.get('TOKEN')]: async (req) => {
+    if (req.method == "POST") {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    return new Response();
+  },
+  '/': () => {
+    return new Response('Hello world!')
+  }
+});
