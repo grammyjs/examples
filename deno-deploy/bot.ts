8c31c17c9c480226a628569f5f3422804fe3183f
Roj <ez@roj.im>
2022-09-04 16:37:56 +0300

    Update Deno Deploy example
    Syncing with the recommendations that lead to https://github.com/grammyjs/website/commit/32f7f81f84b723d4f48a126825308192d3ddfc4e.

--- deno-deploy/bot.ts
+++ deno-deploy/bot.ts
@@ -1,6 +1,6 @@
 import { Bot } from "./deps.deno.ts";
 
-export const bot = new Bot(Deno.env.get("TOKEN") || "");
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
