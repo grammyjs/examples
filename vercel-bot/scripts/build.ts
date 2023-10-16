00f52a99c96100c8869569a716635e2c153f8e97
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-10-17 03:33:14 +0500

    Support for CLI deployment (without branch URL)

--- vercel-bot/scripts/build.ts
+++ vercel-bot/scripts/build.ts
@@ -1,8 +1,8 @@
 import { bot } from '../src/bot'
 
 const {
-    VERCEL_BRANCH_URL: host,
-    // set your webhook address or use default Vercel branch url
+    VERCEL_URL: host,
+    // set your webhook address or use default Vercel deployment url
     WEBHOOK: webhook = `https://${host}/api`,
 } = process.env
 
