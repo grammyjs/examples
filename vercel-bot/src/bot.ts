e95d54a480907df6d58d334f7c809d27e1733411
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2023-10-17 01:58:04 +0500

    Update vercel-bot/src/bot.ts
    Co-authored-by: KnorpelSenf <shtrog@gmail.com>

--- vercel-bot/src/bot.ts
+++ vercel-bot/src/bot.ts
@@ -1,11 +1,11 @@
 import { Bot } from 'grammy'
 
 const {
-    TELEGRAM_BOT_TOKEN: token,
+    TELEGRAM_BOT_TOKEN: token = "",
 } = process.env
 
 // Set your token in the vercel environment variable
-export const bot = new Bot(token as string)
+export const bot = new Bot(token)
 
 // attach all middleware
 bot.on('message', async ctx => {
