c038b2135bc7fc6abca152c4b1ceb8d4117dcc3e
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-10-17 03:34:01 +0500

    Fix for default webhook path

--- vercel-bot/scripts/build.ts
+++ vercel-bot/scripts/build.ts
@@ -3,7 +3,7 @@ import { bot } from '../src/bot'
 const {
     VERCEL_URL: host,
     // set your webhook address or use default Vercel deployment url
-    WEBHOOK: webhook = `https://${host}/api`,
+    WEBHOOK: webhook = `https://${host}/api/webhook`,
 } = process.env
 
 void bot.api.setWebhook(webhook)
